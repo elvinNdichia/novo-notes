113baede6581dc482a0fbf998f0ff635d60b9919
Admin Mboa <adminmboa@Elvin-Ndichia.local>
2023-05-20 19:42:39 +0100

    not found state of note fixed

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -6,23 +6,30 @@ import {
   AutoHeightTextareaBody,
   AutoHeightTextareaTitle,
 } from "./AutoHeightTextarea";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react"; // CHANGED: useContext is imported directly from react
 import { BrainContext } from "../helpers/BrainContext";
-import React from "react";
 import { useNavigate } from "react-router-dom";
+import { nothingFoundSVG } from "../assets/nothingFound";
 export default function Note() {
   const { id } = useParams();
-  const { notes, updateNote, deleteNote } = React.useContext(BrainContext);
+  // CHANGED: useContext is called directly
+  const { notes, updateNote, deleteNote } = useContext(BrainContext);
   const navigate = useNavigate();
 
-  const note = notes.find((note) => note.id === id);
-  const [title, setTitle] = useState(note.title);
-  const [body, setBody] = useState(note.body);
+  // ADDED: useState hooks are called before any other hook or conditional statements
+  const [loading, setLoading] = useState(true);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
+  // ADDED: The note is found inside a useEffect and state is updated accordingly
   useEffect(() => {
-    setTitle(note.title);
-    setBody(note.body);
-  }, [note]);
+    const note = notes.find((note) => note.id === id);
+    if (note) {
+      setTitle(note.title);
+      setBody(note.body);
+    }
+    setLoading(false);
+  }, [id, notes]);
 
   const onDone = () => {
     if (title === "" && body === "") {
@@ -37,6 +44,41 @@ export default function Note() {
     deleteNote(id);
     navigate("/");
   };
+  // Show nothing if loading
+  if (loading) {
+    return;
+  }
+  // The app will not crash if no note is available
+  if (!title && !body) {
+    return (
+      <Box
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+          gridRow: "1/-1",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <div>{nothingFoundSVG}</div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <p
+              className="card-body"
+              style={{
+                color: "#6B6B6B",
+                textAlign: "center",
+                marginTop: "8px",
+              }}
+            >
+              This note does not exist
+            </p>
+          </div>
+        </div>
+      </Box>
+    );
+  }
 
   return (
     <>
