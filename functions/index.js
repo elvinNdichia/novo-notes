90f15fa9ab737d7f4112d7914dce30e3ed3ab6d2
Admin Mboa <adminmboa@Elvin-Ndichia.local>
2023-05-26 18:54:24 +0100

    removed to buggy function which was asking for money

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,6 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.createUserRecord = functions.auth.user().onCreate((user) => {
-  return admin.firestore().collection("blah").doc(user.uid).set({
-    name: user.displayName,
-    email: user.email,
-    created_at: Date.now(),
-    // any other data you want to initialize
-  });
+  return 0;
+  // any other data you want to initialize
 });
