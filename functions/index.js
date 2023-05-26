671766b47f685ff037007d8f480d00adc9136060
Admin Mboa <adminmboa@Elvin-Ndichia.local>
2023-05-26 18:56:30 +0100

    emptied index.js of firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,1 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp();
 
-exports.createUserRecord = functions.auth.user().onCreate((user) => {
-  return 0;
-  // any other data you want to initialize
-});
